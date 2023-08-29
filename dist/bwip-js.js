c7b60b87c27131414caeecd20e8f6ef16c46b422
Mark Warren <mwarren@metafloor.com>
2023-08-29 08:47:53 -0600

    Update to 4.1.0

--- dist/bwip-js.js
+++ dist/bwip-js.js
@@ -41467,14 +41467,15 @@ function DrawingSVG(opts) {
             }
         },
         // Called after all drawing is complete.  The return value from this method
-        // is the return value from `bwipjs.render()`.
+        // will be the return value from `bwipjs.render()`.
         end() {
             var linesvg = '';
             for (var key in lines) {
                 linesvg += lines[key] + '" />\n';
             }
             var bg = opts.backgroundcolor;
-            return '<svg version="1.1" width="' + gs_width + '" height="' + gs_height +
+            //return '<svg version="1.1" width="' + gs_width + '" height="' + gs_height +
+            return '<svg version="1.1" viewBox="0 0 ' + gs_width + ' ' + gs_height +
                         '" xmlns="http://www.w3.org/2000/svg">\n' +
 						(clips.length ? '<defs>' + clips.join('') + '</defs>' : '') +
                         (/^[0-9A-Fa-f]{6}$/.test(''+bg)
