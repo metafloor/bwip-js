ce70281cda9aa86fc7002865c3489c3b134b5191
Mark Warren <mwarren@metafloor.com>
2026-03-31 18:22:00 -0600

    Update to 4.9.0

--- demo/src/exports.js
+++ demo/src/exports.js
@@ -443,23 +443,15 @@ function ToRaw(bcid, text, options) {
 
     // bwip-js uses Maps to emulate PostScript dictionary objects; but Maps
     // are not a typical/expected return value.  Convert to plain-old-objects.
-    var ids = { pixs:1, pixx:1, pixy:1, sbs:1, bbs:1, bhs:1, width:1, height:1 };
+    var ids = { pixs:1, pixx:1, pixy:1, sbs:1, bbs:1, bhs:1, txt:1, width:1, height:1 };
     for (var i = 0; i < stack.length; i++) {
         var elt = stack[i];
         if (elt instanceof Map) {
             var obj = {};
-            // Could they make Maps any harder to iterate over???
             for (var keys = elt.keys(), size = elt.size, k = 0; k < size; k++) {
                 var id = keys.next().value;
                 if (ids[id]) {
-                    var val = elt.get(id);
-                    if (val instanceof Array) {
-                        // The postscript arrays have extra named properties
-                        // to emulate array views.  Return cleaned up arrays.
-                        obj[id] = val.b.slice(val.o, val.o + val.length);
-                    } else {
-                        obj[id] = val;
-                    }
+                    obj[id] = pod(elt.get(id));
                 }
             }
             stack[i] = obj;
@@ -469,4 +461,20 @@ function ToRaw(bcid, text, options) {
         }
     }
     return stack;
+
+    function pod(val) {
+        if (val instanceof Array) {
+            // The postscript arrays have extra named properties
+            // to emulate array views.  Return cleaned up arrays.
+            val = val.b.slice(val.o, val.o + val.length);
+
+            // Walk the array and convert each element
+            for (let j = 0, l = val.length; j < l; j++) {
+                val[j] = pod(val[j]);
+            }
+        } else if (val instanceof Uint8Array) {
+            val = String.fromCharCode.apply(null, val);
+        }
+        return val;
+    }
 }
